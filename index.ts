db110c382d70d2f3575dceebcd87c511ae07b804
eric_wang <jadeface.scholar@gmail.com>
2021-03-31 02:02:21 +0800

    feat: 完善计数

--- index.ts
+++ index.ts
@@ -9,6 +9,15 @@ const config = require('./ss.config.js') || {};
 
 const spinner = ora('Read config from file and shell');
 
+var showMem = function() {
+    var mem = process.memoryUsage();
+    var format = function(bytes) {
+        return (bytes / 1024 / 1024).toFixed(2) + ' MB';
+    };
+    console.log('Process: heapTotal ' + format(mem.heapTotal) + ' heapUsed ' + format(mem.heapUsed) + ' rss ' + format(mem.rss));
+    console.log('-----------------------------------------------------------');
+};
+
 
 async function run(source, compileLib) {
     spinner.start();
@@ -81,6 +90,7 @@ async function run(source, compileLib) {
                             `))
 
                             console.log(Date.now() - startTime+'s')
+                            showMem();
                         }
                     })
                 }

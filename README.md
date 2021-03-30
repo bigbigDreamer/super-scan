30e00bbfa02d10e6039c07ae06b9b0e4ef8f10e1
eric_wang <jadeface.scholar@gmail.com>
2021-03-31 01:52:57 +0800

    feat: 进程-线程通信

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Super Scan
 
-超级扫描，一键扫描替换。
+## 背景
+
+项目需要变量化，但是开发的时候，关心变量化会降低额外的开发效率。
+
+通常的模式都是
+
 
 ## 参考文档
 
@@ -8,3 +13,8 @@
 
 【2】[从 1 到完美，用 node 写一个命令行工具](https://segmentfault.com/a/1190000016555129)
 
+【3】[Node.js CLI Apps Best Practices](https://github.com/lirantal/nodejs-cli-apps-best-practices/tree/3afe1ab0a5b506ef8c32903c4bf253a4cdb4bddd)
+
+【4】[Node.js v15.12.0文档](https://nodejs.org/api/fs.html)
+
+【5】[nodejs真的是单线程吗？](https://segmentfault.com/a/1190000014926921)
--- index.ts
+++ index.ts
@@ -3,17 +3,22 @@ const { program } = require('commander');
 const fs = require('fs');
 const ora = require('ora');
 const chalk = require('chalk');
-const { superScan } = require('./lib');
+const { fork } = require('child_process');
 const { parseLessVariable } = require('./lib/parse');
 const config = require('./ss.config.js') || {};
+
+
+const worker = fork('./lib/index.js');
 
 const spinner = ora('Read config from file and shell');
 
 
 async function run(source, compileLib) {
     spinner.start();
+    const startTime = Date.now();
 
     let test = 'less';
+
     const ssAnalysis = {
         total: 0,
         success: 0,
@@ -27,23 +32,31 @@ async function run(source, compileLib) {
 
     try {
         const lessConfigObj = parseLessVariable(source);
-        spinner.succeed('Read config success！Start scan！')
-        superScan({
+        spinner.succeed('Read config success！Start scan！');
+
+        await Promise.resolve();
+
+        /** 转入异步调度 */
+        worker.send({
             filePath: compileLib,
             test,
-            onSuccess: (data, fileDir) => {
-                ssAnalysis.total+=1;
-                let newData = data;
-                Object.keys(lessConfigObj).forEach(item => {
-                    const regx = new RegExp(lessConfigObj[item], 'g')
-                    if (regx.test(newData)) {
-                        newData = newData.replace( regx, item);
-                    }
-                });
+            // onSuccess
+        })
 
-                if (newData) {
-                    spinner.start('Start overwrite source file！')
-                    fs.writeFile(fileDir, newData, {},(err) => {
+        worker.on('message', ({ data, fileDir, pid }) => {
+            console.log(data, fileDir, "dd", pid)
+            ssAnalysis.total+=1;
+            let newData = data;
+            Object.keys(lessConfigObj).forEach(item => {
+                const regx = new RegExp(lessConfigObj[item], 'g')
+                if (regx.test(newData)) {
+                    newData = newData.replace(regx, item);
+                }
+            });
+
+            if (newData) {
+                spinner.start('Start overwrite source file！')
+                fs.writeFile(fileDir, newData, {},(err) => {
                         if (err) {
                             spinner.fail(`File: ${chalk.redBright(`${fileDir}`)} overwrite fail, view detail err`);
                             ssAnalysis.fail+=1;
@@ -54,35 +67,39 @@ async function run(source, compileLib) {
 
                         if (ssAnalysis.fail + ssAnalysis.success === ssAnalysis.total) {
                             console.log(chalk.green(`
-                                        GOOD JOB！！！
-                                        
-                                           X
-                                        YYYYYYY
-                                      N777777777NO
-                                    N7777777777777N
-                                     DNNM$$$$777777N                              D
-                                    N$N:=N$777N7777M                             NZ
-                                   77Z::::N777777777                          ODZZZ
-                                   D=::::::::::N7777777777N                    777
-                                  INN===::::::=77777777777N                  I777N
-                                 ?777N========N7777777777787M               N7777
-                                 77777$D======N77777777777N777N?         N777777
-                                I77777$$$N7===M$$77777777$77777777$MMZ77777777N
-                                 $$$$$$$$$$$NIZN$$$$$$$$$M$$7777777777777777ON
-                                  M$$$$$$$$M    M$$$$$$$$N=N$$$$7777777$$$ND
-                                 O77Z$$$$$$$     M$$$$$$$$MNI==$DNNNNM=~N
-                              7 :N MNN$$$$M$      $$$777$8      8D8I
-                                NMM.:7O           777777778
-                                                  M   :   M
-                                                       8
-   
-                                The Files: ${chalk.blue(`Total(${ssAnalysis.total})`)}, Success(${ssAnalysis.success}), ${chalk.red(`Fail(${ssAnalysis.fail})`)}
-                            `))
+                                            GOOD JOB！！！
+
+                                               X
+                                            YYYYYYY
+                                          N777777777NO
+                                        N7777777777777N
+                                         DNNM$$$$777777N                              D
+                                        N$N:=N$777N7777M                             NZ
+                                       77Z::::N777777777                          ODZZZ
+                                       D=::::::::::N7777777777N                    777
+                                      INN===::::::=77777777777N                  I777N
+                                     ?777N========N7777777777787M               N7777
+                                     77777$D======N77777777777N777N?         N777777
+                                    I77777$$$N7===M$$77777777$77777777$MMZ77777777N
+                                     $$$$$$$$$$$NIZN$$$$$$$$$M$$7777777777777777ON
+                                      M$$$$$$$$M    M$$$$$$$$N=N$$$$7777777$$$ND
+                                     O77Z$$$$$$$     M$$$$$$$$MNI==$DNNNNM=~N
+                                  7 :N MNN$$$$M$      $$$777$8      8D8I
+                                    NMM.:7O           777777778
+                                                      M   :   M
+                                                           8
+
+                                    The Files: ${chalk.blue(`Total(${ssAnalysis.total})`)}, Success(${ssAnalysis.success}), ${chalk.red(`Fail(${ssAnalysis.fail})`)}
+                                `))
+                            console.log(Date.now() - startTime+'s');
+                            process.kill(pid);
                         }
-                    })
-                }
+
+                    }
+                )
             }
-        });
+        })
+
 
     } catch (e) {
         spinner.fail(`解析出错! ${e.toString()}`)
@@ -102,3 +119,12 @@ async function main() {
 }
 
 main().catch(err => console.error(err));
+
+
+// 当进程出现会崩溃的错误 子进程捕获uncautht异常，并主动退出；
+process.on('uncaughtException', function (err) {
+    // 这里可以做写日志的操作
+    console.log(err);
+    // 退出进程
+    process.exit();
+});
